2859063987ba545bf46ee684cd410fda9650243a
Muhammad Amir <amrafridi.29@gmail.com>
2026-03-13 14:51:54 +0500

    cleanup

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "terraform-state-bucket-26032028"
-    key          = "terraform.tfstate"
-    region       = "us-east-2"
-    use_lockfile = true
-  }
-}
--- variables.tf
+++
@@ -1,5 +0,0 @@
-variable "region" {
-  description = "The region to deploy the infrastructure"
-  type        = string
-  default     = "us-east-2"
-}
