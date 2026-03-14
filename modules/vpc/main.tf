af8a52ddeae9d15a9be7b5da8e3ba4abed32f83e
Muhammad Amir <amrafridi.29@gmail.com>
2026-03-14 17:31:50 +0500

    feat: add private route table

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -91,3 +91,25 @@ resource "aws_route_table_association" "public_association" {
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
+
+resource "aws_route_table" "private" {
+  count  = length(var.private_subnets)
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "${var.name}-private-rt-${count.index + 1}"
+  }
+}
+
+resource "aws_route" "private_nat" {
+  count                  = length(var.private_subnets)
+  route_table_id         = aws_route_table.private[count.index].id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat[count.index].id
+}
+
+resource "aws_route_table_association" "private_association" {
+  count          = length(aws_subnet.private)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
+}
