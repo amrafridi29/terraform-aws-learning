e9d3978710ff44400f8df8f7bb44ecce4166be9e
Muhammad Amir <amrafridi.29@gmail.com>
2026-03-15 05:34:16 +0500

    wip

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -1,11 +1,9 @@
 output "web_instance_ids" {
-  value = [for instance in aws_instance.web : instance.id]
+  value = aws_instance.web[*].id
 }
-
 output "app_instance_ids" {
-  value = [for instance in aws_instance.app : instance.id]
+  value = aws_instance.app[*].id
 }
-
 output "web_public_ips" {
-  value = [for instance in aws_instance.web : instance.public_ip]
+  value = aws_instance.web[*].public_ip
 }
