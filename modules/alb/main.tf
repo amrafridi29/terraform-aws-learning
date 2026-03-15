8191169352d3ee3ee3f5c4617ba988ed76269ef9
Muhammad Amir <amrafridi.29@gmail.com>
2026-03-15 06:19:49 +0500

    feat:add health check in web tg

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -18,6 +18,16 @@ resource "aws_lb_target_group" "web" {
   protocol = "HTTP"
   vpc_id   = var.vpc_id
 
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200-399"
+  }
+
   tags = {
     Name = "${var.name}-web-tg"
   }
